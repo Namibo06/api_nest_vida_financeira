9040aea6b986c9554ed443af312549c141f8085b
Namibo06 <namibo654@gmail.com>
2024-12-05 13:06:49 -0300

    feat: create interface life financial

---
+++ src/interfaces/LifeFinancialInterface.ts
@@ -0,0 +1,14 @@
+
+import { CreateLifeFinancialDTO } from "src/dtos/life_financial/CreateLifeFinancialDTO";
+import { UpdateLifeFinancialDTO } from "src/dtos/life_financial/UpdateLifeFinancialDTO";
+import { MessageStatusDTO } from "src/dtos/user/MessageStatusDTO";
+import { LifeFinancial } from "src/schemas/life_financial.schema";
+
+export interface LifeFinancialInterface{
+    create(data: CreateLifeFinancialDTO): Promise<MessageStatusDTO>;
+    getAll(): Promise<LifeFinancial[]>;
+    getOne(id: string): Promise<LifeFinancial>;
+    update(id: string, data: UpdateLifeFinancialDTO): Promise<MessageStatusDTO>;
+    delete(id: string):Promise<MessageStatusDTO>;
+    existsById(id: string): Promise<Boolean>;
+}
