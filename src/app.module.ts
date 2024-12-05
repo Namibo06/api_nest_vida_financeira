1ed40a97a808894cf35cff5c6910e1fbe2f5fc4f
Namibo06 <namibo654@gmail.com>
2024-12-05 00:11:37 -0300

    chore: added schemas in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/user.schema';
+import { ItemSchema } from './schemas/item.schema';
+import { LifeFinancialSchema } from './schemas/life_financial.schema';
+import { FinancialSchema } from './schemas/financial.schema';
+import { GoalsSchema } from './schemas/goals.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:financas_tech')
+    MongooseModule.forRoot('mongodb://localhost:financas_tech'),
+    MongooseModule.forFeature([{name: "UserSchema", schema: UserSchema}]),
+    MongooseModule.forFeature([{name: "ItemSchema", schema: ItemSchema}]),
+    MongooseModule.forFeature([{name: "LifeFinancialSchema", schema: LifeFinancialSchema}]),
+    MongooseModule.forFeature([{name: "FinancialSchema", schema: FinancialSchema}]),
+    MongooseModule.forFeature([{name: "GoalsSchema", schema: GoalsSchema}]),
   ],
   controllers: [AppController],
   providers: [AppService],
