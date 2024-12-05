be4cb2d8895b784c3cd2986a67bcfebea899752c
Namibo06 <namibo654@gmail.com>
2024-12-05 13:02:45 -0300

    feat: create interface for item

---
+++ src/interfaces/ItemInterface.ts
@@ -0,0 +1,14 @@
+
+import { CreateItemDTO } from "src/dtos/item/CreateItemDTO";
+import { UpdateItemDTO } from "src/dtos/item/UpdateItemDTO";
+import { MessageStatusDTO } from "src/dtos/user/MessageStatusDTO";
+import { Item } from "src/schemas/item.schema";
+
+export interface ItemInterface{
+    create(data: CreateItemDTO): Promise<MessageStatusDTO>;
+    getAll(): Promise<Item[]>;
+    getOne(id: string): Promise<Item>;
+    update(id: string, data: UpdateItemDTO): Promise<MessageStatusDTO>;
+    delete(id: string):Promise<MessageStatusDTO>;
+    existsById(id: string): Promise<Boolean>;
+}
