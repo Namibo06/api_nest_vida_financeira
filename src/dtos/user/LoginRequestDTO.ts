35dcfc8c5bc26d7340433752b7504c15fdff6579
Namibo06 <namibo654@gmail.com>
2024-12-05 13:03:46 -0300

    feat: DTO for request login

---
+++ src/dtos/user/LoginRequestDTO.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class LoginRequestDTO{
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+}
