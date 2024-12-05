1896f0ecdcfc917e2eac876fb2e37584b0fd8f13
Namibo06 <namibo654@gmail.com>
2024-12-05 00:10:31 -0300

    feat: create item schema

---
+++ src/schemas/item.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
+import { OperationItem } from "src/enums/operation.enum";
+
+@Schema()
+export class ItemSchema{
+    @Prop({required: true, maxlength: 50})
+    name: string;
+
+    @Prop({required: true})
+    operation: OperationItem;
+
+    @Prop({required: true})
+    price: number;
+}
+
+export const itemSchema = SchemaFactory.createForClass(ItemSchema);
