04a86cbf44db05bfae1737b3c3d8312a56c991e7
Namibo06 <namibo654@gmail.com>
2024-12-06 00:35:15 -0300

    feat: create goals service

---
+++ src/services/GoalsService.ts
@@ -0,0 +1,36 @@
+import { CreateGoalsDTO } from "src/dtos/goals/CreateGoalsDTO";
+import { UpdateGoalsDTO } from "src/dtos/goals/UpdateGoalsDTO";
+import { MessageStatusDTO } from "src/dtos/user/MessageStatusDTO";
+import { GoalsInterface } from "src/interfaces/GoalsInterface";
+import { GoalsRepository } from "src/repositories/GoalsRepository";
+import { Goals } from "src/schemas/goals.schema";
+
+export class GoalsService implements GoalsInterface{
+    constructor(
+        private repository: GoalsRepository
+    ){}
+
+    async create(data: CreateGoalsDTO): Promise<MessageStatusDTO> {
+       return await this.repository.create(data);
+    }
+    
+    async getAll(): Promise<Goals[]> {
+        return await this.repository.getAll();
+    }
+    
+    async getOne(id: string): Promise<Goals> {
+        return await this.repository.getOne(id);
+    }
+    
+    async update(id: string, data: UpdateGoalsDTO): Promise<MessageStatusDTO> {
+        return await this.repository.update(id,data);
+    }
+    
+    async delete(id: string): Promise<MessageStatusDTO> {
+        return await this.repository.delete(id);
+    }
+    
+    async existsById(id: string): Promise<Boolean> {
+        return await this.repository.existsById(id);
+    }  
+}
