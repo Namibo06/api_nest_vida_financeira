b440e872348a6775f00f4a624bef5934260e46c4
Namibo06 <namibo654@gmail.com>
2024-12-10 22:52:19 -0300

    refactor: added else

--- src/exceptions/GlobalExceptionFilter.ts
+++ src/exceptions/GlobalExceptionFilter.ts
@@ -1,26 +1,30 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
 import { HandleGlobalException } from 'src/utils/HandleGlobalException';
 import { NotFoundException } from './NotFoundException';
 import { InternalServerErrorException } from './InternalServerErrorException';
 import { UnprocessableEntityException } from './UnprocessableEntityException';
 
 @Catch()
-export class GlobalExceptionFilter implements ExceptionFilter{
-    catch(exception: unknown, host: ArgumentsHost) {
-        const ctx = host.switchToHttp();
-        const response = ctx.getResponse();
+export class GlobalExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
 
-        let customResponse: { status: number| string ; message: string; };
+    let customResponse: { status: number | string; message: string };
 
-        if(exception instanceof NotFoundException){
-            customResponse = HandleGlobalException.handleNotFoundException(exception);
-        }else if(exception instanceof InternalServerErrorException){
-            customResponse = HandleGlobalException.handleInternalServerErrorException(exception);
-        }else if(exception instanceof UnprocessableEntityException){
-            customResponse = HandleGlobalException.handleUnprocessableEntityException(exception);
-        }
-
-        response.status(customResponse.status).json(customResponse);
+    if (exception instanceof NotFoundException) {
+      customResponse = HandleGlobalException.handleNotFoundException(exception);
+    } else if (exception instanceof InternalServerErrorException) {
+      customResponse = HandleGlobalException.handleInternalServerErrorException(exception);
+    } else if (exception instanceof UnprocessableEntityException) {
+      customResponse = HandleGlobalException.handleUnprocessableEntityException(exception);
+    } else {
+      customResponse = {
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Erro inesperado',
+      };
     }
-    
+
+    response.status(Number(customResponse.status)).json(customResponse);
+  }
 }
