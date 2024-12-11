ef260cd8b6a4485e0e81553b37a6329cb7da4450
Namibo06 <namibo654@gmail.com>
2024-12-11 10:37:56 -0300

    refactor: added modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,25 @@
-import { Module, OnModuleInit } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { userSchema } from './schemas/user.schema';
-import { itemSchema } from './schemas/item.schema';
-import { lifeFinancialSchema } from './schemas/life_financial.schema';
-import { financialSchema } from './schemas/financial.schema';
-import { goalsSchema } from './schemas/goals.schema';
+import { UserModule } from './modules/user/user.module';
+import { LifeFinancialModule } from './modules/life-financial/life-financial.module';
+import { FinancialModule } from './modules/financial/financial.module';
+import { ItemModule } from './modules/item/item.module';
+import { GoalsModule } from './modules/goals/goals.module';
 
 require('dotenv').config();
 
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
-      useFactory: async () => {
-        try {
-          console.log('Attempting to connect to MongoDB...');
-          return {
-            uri: process.env.MONGODB_URI,
-          };
-        } catch (error) {
-          console.error('Error connecting to MongoDB:', error);
-          throw error; 
-        }
-      },
-    }),
-    MongooseModule.forFeature([{name: "User", schema: userSchema}]),
-    MongooseModule.forFeature([{name: "Item", schema: itemSchema}]),
-    MongooseModule.forFeature([{name: "LifeFinancial", schema: lifeFinancialSchema}]),
-    MongooseModule.forFeature([{name: "Financial", schema: financialSchema}]),
-    MongooseModule.forFeature([{name: "Goals", schema: goalsSchema}]),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    UserModule,
+    LifeFinancialModule,
+    FinancialModule,
+    ItemModule,
+    GoalsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule{
-
-}
+export class AppModule {}
