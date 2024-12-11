9da07a8e3815f6456b7a84b225babfed6d3cc5c2
Namibo06 <namibo654@gmail.com>
2024-12-11 13:58:50 -0300

    feat: create methods and others changes user use case

--- src/use_cases/UserUseCase.ts
+++ src/use_cases/UserUseCase.ts
@@ -8,24 +8,39 @@ import { NotFoundException } from "src/exceptions/NotFoundException";
 import { UnprocessableEntityException } from "src/exceptions/UnprocessableEntityException";
 import { User } from "src/schemas/user.schema";
 import { UserService } from "src/services/User.service";
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class UserUseCase{
     constructor(
-        private readonly service: UserService
+        private readonly service: UserService,
+        private readonly jwtService: JwtService
     ){}
 
     async createUserUseCase(data: CreateUserDTO): Promise<MessageStatusDTO>{
-        console.log("passei na use case antes do if");
         if(
-            data.email !== "" || data.email !== null ||
-            data.nickname !== "" || data.nickname !== null ||
-            data.password !== "" || data.password !== null 
+            data.email === "" || data.email === null ||
+            data.nickname === "" || data.nickname === null ||
+            data.password === null 
         ){
             throw new UnprocessableEntityException("Campos obrigatórios vazios");
         }
 
-        return await this.service.create(data);
+        const { password } = data;
+        const salts = 10;
+
+        try{
+            const encryptedPassword = await bcrypt.hash(password, salts);
+
+            const newData = new CreateUserDTO(
+                data.nickname,data.email,encryptedPassword
+            );
+
+            return await this.service.create(newData);
+        }catch(error){
+            console.log('error: '+error)
+        }
     }
 
     async getAllUsersUseCase(): Promise<User[]>{
@@ -59,18 +74,27 @@ export class UserUseCase{
         return await this.service.delete(id);
     }
 
-    async loginUseCase(data: LoginRequestDTO): Promise<LoginResponseDTO>{
+    async loginUseCase(data: LoginRequestDTO): Promise<LoginResponseDTO | MessageStatusDTO>{
         if(
-            data.email !== "" || data.email !== null ||
-            data.password !== "" || data.password !== null 
+            data.email === "" || data.email === null ||
+            data.password === "" || data.password === null 
         ){
             throw new UnprocessableEntityException("Campos obrigatórios vazios");
         }
 
-        //fazer verificação de email
-        //fazer verificação de senha
-        //gerar token
+        await this.service.login(data);
+        const getIdAndNickname = await this.service.getOneByEmail(data.email);
+        const payload = {
+            sub: getIdAndNickname._id,
+            username: getIdAndNickname.nickname
+        }
 
-        return await this.service.login(data);
+        const token = await this.jwtService.signAsync(payload);
+
+        return new LoginResponseDTO(
+            "Login realizado com sucesso",
+            200,
+            token
+        );
     }
 }
